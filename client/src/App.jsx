e478392cf8f5435771ba8a6b553f46171b11371a
lia Ha <fayyyufei@gmail.com>
2024-06-27 15:35:06 -0700

    add headler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from'react'
-
+import {BrowserRouter, Route} from'react-router-dom';
+import Home from './pages/Home';
 export default function App(){
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<Home />}/>
+      <Route path = "/" element = {<About />}/>
+      <Route path = "/" element = {<Signin />}/>
+      <Route path = "/" element = {<SignUp />}/>
+      <Route path = "/" element = {<Profile />}/>
+    </Routes>
+    </BrowserRouter>
+
 }
